09e4bb43ffce1ef6b1ad4acd5efcfc7dd47c86f6
Sushant-Luitel <sushantluitel39@gmail.com>
2024-02-21 18:15:47 +0545

    render the state of count variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
-  return <>Hello World</>;
+  const [count, setCount] = useState(0);
+
+  const handleAdd = () => {
+    setCount((count) => count + 1);
+  };
+  const handleSub = () => {
+    setCount((count) => count - 1);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <p>{count}</p>
+        <button className="add-button" onClick={handleAdd}>
+          Add
+        </button>
+        <button
+          className="sub-button"
+          onClick={handleSub}
+          disabled={count === 0 ? true : false}
+        >
+          Subtract
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
