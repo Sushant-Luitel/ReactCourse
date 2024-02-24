1c694f304a2584c29bc455082feac7e70a3cefeb
Sushant-Luitel <sushantluitel39@gmail.com>
2024-02-24 15:07:13 +0545

    added select option in the form

--- src/Components/AddItem.jsx
+++ src/Components/AddItem.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
 import "./AddItem.css";
 const AddItem = () => {
-  const [inputValue, setInputValue] = useState();
+  const [inputValue, setInputValue] = useState("");
+  const [progress, setProgress] = useState(false);
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
+  function handleSubmit(e) {
+    e.preventDefault();
+    const task = {
+      id: Math.floor(Math.random() * 1000000),
+      name: inputValue,
+      completed: Boolean(progress),
+    };
+    console.log(task);
+    ResetValue();
+  }
+
   const ResetValue = () => {
     setInputValue("");
+    setProgress(false);
   };
 
   return (
     <section className="add-item container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="Name">Name</label>
         <input
           value={inputValue}
@@ -22,7 +35,13 @@ const AddItem = () => {
           placeholder="Enter your name"
           autoComplete="off"
         />
-        <button className="sub-button">Add Task</button>
+        <select onChange={(e) => setProgress(e.target.value)} value={progress}>
+          <option value="false">Pending</option>
+          <option value="true">Completed</option>
+        </select>
+        <button type="submit" className="sub-button">
+          Add Task
+        </button>
         <span className="reset add-button" onClick={ResetValue}>
           Reset
         </span>
